be92be005e763e90953023e2274bcb45d82d93c4
bfforeverli <lipengios@163.com>
2015-12-24 16:49:41 +0800

    增加控件点击获取

--- RYReachability/RYReachability/ViewController.m
+++ RYReachability/RYReachability/ViewController.m
@@ -73,6 +73,51 @@
     
     
 }
+- (IBAction)getNewNetState:(UIButton *)sender {
+    
+    NetWorkSatusType type =  [[RYPreferanceManager sharePreferanceManager]currentNetStatusType];
+    switch (type) {
+        case NetWorkSatusType_None:
+        {
+            self.currentNetWorkStatus.text = @"无网络连接";
+            NSLog(@"没有网络");
+        }
+            break;
+        case NetWorkSatusType_WiFi:{
+            self.currentNetWorkStatus.text = @"当前连接是WiFi";
+            NSLog(@" current net is wifi");
+        }
+            break;
+        case NetWorkSatusType_2G:{
+            self.currentNetWorkStatus.text = @"当前使用的是2G网络";
+            NSLog(@" current net is 2g");
+            
+        }
+            break;
+        case NetWorkSatusType_3G:{
+            self.currentNetWorkStatus.text = @"当前使用的是3g网络";
+            NSLog(@" current net is 3g");
+            
+        }
+            break;
+        case NetWorkSatusType_4G:{
+            self.currentNetWorkStatus.text = @"当前使用的是4G网络";
+            NSLog(@" current net is 4g");
+            
+        }
+            break;
+        case NetWorkSatusType_5G:{
+            NSLog(@" current net is 5g  暂时还未找到支持5G的API");
+            
+        }
+            break;
+            
+            
+        default:
+            break;
+    }
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
